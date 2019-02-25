606b07179d5e90d65557dce69df254648a922913
yminchev <44697684+yminchev@users.noreply.github.com>
2019-02-25 11:13:32 +0200

    Update django-deploy.sh

--- django-deploy.sh
+++ django-deploy.sh
@@ -14,15 +14,11 @@ else
   INAME='yminc.com:5000/mydjango:latest'
 fi
 
-
-echo -n "Stopping old container $CNAME..."
+echo "Stopping old container $CNAME..."
 docker stop $CNAME
-echo "Done."
 
-echo -n "Removing old container $CNAME..."
+echo "Removing old container $CNAME..."
 docker rm $CNAME
-echo "Done."
 
-echo -n "Starting a new container $CNAME from $INAME..."
+echo "Starting a new container $CNAME from $INAME..."
 docker run -d -p 8000:8000 --name=django yminc.com:5000/mydjango:latest 
-echo "Done."
